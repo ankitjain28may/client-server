9540f241de596e48b91345e3d56a051ff8284561
ankitjain28may <ankitjain28may77@gmail.com>
2021-01-16 19:04:59 +0530

    Implemented concurrency by using threads in python

--- server.py
+++ server.py
@@ -4,37 +4,43 @@ from threading import Thread
 import json
 
 # Mutli thread for concurrency
-# class ClientConcurrency(Thread):
+class ClientConcurrency(Thread):
 
-#     def __init__(self, add):
-#         Thread.__init__(self)
-#         print("New thread started for client Address: ", add)
+    def __init__(self, conn, add):
+        self.conn = conn
+        self.add = add
+        Thread.__init__(self)
+        print("New thread started for client Address: ", self.add)
     
-#     def run(self):
-#         while True:
-#             try:
-#                 data = conn.recv(1024)
-#                 if not data: break
-#                 data = data.decode("utf-8")
-#                 s = shlex.split(data)
-#                 if s[0] == "set" and len(s) >= 2:
-#                     dic[s[1]] = s[2]
-#                     conn.send(bytes("The value is set successfully against the key: " + s[1], "utf-8"))
-#                 elif s[0] == "get" and len(s) >= 1:
-#                     if s[1] in dic:
-#                         conn.send(bytes(dic[s[1]], "utf-8"))
-#                     else:
-#                         conn.send(bytes("Not found", "utf-8"))
-#                 elif s[0] == "delete":
-#                     dic.pop(s[1])
-#                     conn.send(bytes("key is deleted", "utf-8"))
-#                 elif s[0] == "exit":
-#                     conn.close()
-#                 else:
-#                     conn.send(bytes("Unknown operation", "utf-8"))
-#                 # conn.send(data)
-#             except Exception as e:
-#                 conn.send(bytes("Error: " + str(e), "utf-8"))
+    def run(self):
+        while True:
+            try:
+                data = self.conn.recv(1024)
+                if not data: return
+                data = data.decode("utf-8")
+                s = shlex.split(data)
+                if s[0] == "set" and len(s) >= 2:
+                    dic[s[1]] = s[2]
+                    write_data()
+                    self.conn.send(bytes("The value is set successfully against the key: " + s[1], "utf-8"))
+                elif s[0] == "get" and len(s) >= 1:
+                    if s[1] in dic:
+                        self.conn.send(bytes(dic[s[1]], "utf-8"))
+                    else:
+                        self.conn.send(bytes("Not found", "utf-8"))
+                elif s[0] == "delete":
+                    dic.pop(s[1])
+                    write_data()
+                    self.conn.send(bytes("key is deleted", "utf-8"))
+                elif s[0] == "exit":
+                    self.conn.close()
+                    print("Connection is closed for client Address: ", self.add)
+                    return
+                else:
+                    self.conn.send(bytes("Unknown operation", "utf-8"))
+                # self.conn.send(data)
+            except Exception as e:
+                self.conn.send(bytes("Error: " + str(e), "utf-8"))
 
 
 def write_data():
@@ -69,38 +75,10 @@ with open('abc.json', 'r') as fi:
 while True:
     conn, add = sock.accept()
     print("Address: ", add)
-    # try:
-        # newClientThread = ClientConcurrency(add)
-        # newClientThread.start()
-    # except Exception as e:
-    #     print("Error: " + str(e))
-
-    while True:
-        try:
-            data = conn.recv(1024)
-            if not data: break
-            data = data.decode("utf-8")
-            s = shlex.split(data)
-            if s[0] == "set" and len(s) >= 2:
-                dic[s[1]] = s[2]
-                write_data()
-                conn.send(bytes("The value is set successfully against the key: " + s[1], "utf-8"))
-            elif s[0] == "get" and len(s) >= 1:
-                if s[1] in dic:
-                    conn.send(bytes(dic[s[1]], "utf-8"))
-                else:
-                    conn.send(bytes("Not found", "utf-8"))
-            elif s[0] == "delete":
-                dic.pop(s[1])
-                write_data()
-                conn.send(bytes("key is deleted", "utf-8"))
-            elif s[0] == "exit":
-                conn.close()
-            else:
-                conn.send(bytes("Unknown operation", "utf-8"))
-            # conn.send(data)
-        except Exception as e:
-            conn.send(bytes("Error: " + str(e), "utf-8"))
-        
+    try:
+        newClientThread = ClientConcurrency(conn, add)
+        newClientThread.start()
+    except Exception as e:
+        print("Error: " + str(e))
 # Closing the socket
 sock.close()
